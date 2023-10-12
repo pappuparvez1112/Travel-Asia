bb3e8519e6684ade63ee1aa200eca47e099805f6
pappuparvez1112 <pappuparvezshazz@gmail.com>
2023-10-13 02:15:32 +0600

    [Done] project set up and auth service

--- src/app/Modules/User/user.route.ts
+++ src/app/Modules/User/user.route.ts
@@ -4,7 +4,7 @@ import auth from '../../middlewares/auth';
 import { UserController } from './user.controller';
 
 const router = express.Router();
-router.get('/', auth(ENUM_USER_ROLE.ADMIN), UserController.getAllUsers);
+router.get('/', UserController.getAllUsers);
 router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), UserController.getSingleUser);
 router.patch(
   '/:id',
